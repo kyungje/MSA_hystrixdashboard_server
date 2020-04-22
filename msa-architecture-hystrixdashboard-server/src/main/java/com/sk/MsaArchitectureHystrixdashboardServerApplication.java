0bbe550fff7d850a08207e45daa54edc702510c7
kyungje <kyungje@gmail.com>
2020-04-22 20:12:19 +0900

    init

---
+++ msa-architecture-hystrixdashboard-server/src/main/java/com/sk/MsaArchitectureHystrixdashboardServerApplication.java
@@ -0,0 +1,18 @@
+package com.sk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableHystrixDashboard
+@EnableEurekaClient
+@SpringBootApplication
+public class MsaArchitectureHystrixdashboardServerApplication {
+
+//	http://localhost:10004/turbine.stream Á¢¼Ó
+	public static void main(String[] args) {
+		SpringApplication.run(MsaArchitectureHystrixdashboardServerApplication.class, args);
+	}
+
+}
